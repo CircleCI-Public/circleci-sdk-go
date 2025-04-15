9f4f29982dd2821f7ecf8cfeac6354e028e4865e
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-04-15 13:38:22 -0500

    Add method for create project and code organization

--- client/client.go
+++ client/client.go
@@ -22,8 +22,7 @@ func NewClient(baseURL, authToken string) *Client {
 	}
 }
 
-func (c *Client) RequestHelper(method, path string, body any) (*http.Response, error) {
-	url := c.BaseURL + path
+func requestHelperFunction(url, token, method string, body any, client *http.Client) (*http.Response, error) {
 	var reqBody io.Reader
 	if body != nil {
 		jsonData, err := json.Marshal(body)
@@ -38,9 +37,9 @@ func (c *Client) RequestHelper(method, path string, body any) (*http.Response, e
 	}
 	req.Header.Set("Accept", "application/json")
 	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Circle-Token", c.AuthToken)
+	req.Header.Set("Circle-Token", token)
 
-	res, err := c.HTTPClient.Do(req)
+	res, err := client.Do(req)
 	if err != nil {
 		return nil, err
 	}
@@ -53,6 +52,14 @@ func (c *Client) RequestHelper(method, path string, body any) (*http.Response, e
 		}
 		return nil, fmt.Errorf("%s: %s", res.Status, string(body))
 	}
-
 	return res, nil
+}
+
+// This is the same as RequestHelper but allows to do a request to other APIs
+func (c *Client) RequestHelperAbsolute(method, path string, body any) (*http.Response, error) {
+	return requestHelperFunction(path, c.AuthToken, method, body, c.HTTPClient)
+}
+
+func (c *Client) RequestHelper(method, path string, body any) (*http.Response, error) {
+	return requestHelperFunction(c.BaseURL + path, c.AuthToken, method, body, c.HTTPClient)
 }
--- tests/project_test.go
+++ tests/project_test.go
@@ -29,7 +29,7 @@ func TestGetProject(t *testing.T) {
 	}
 }
 
-func TestGetProjectSettings(t *testing.T) {
+func TestFullProject(t *testing.T) {
 	token := os.Getenv("CIRCLECI_CLI_TOKEN")
 	if token == "" {
 		t.Error("Error: Token not found")
@@ -37,11 +37,77 @@ func TestGetProjectSettings(t *testing.T) {
 	client := client.NewClient("https://circleci.com/api/v2", token)
 	projectService := project.NewProjectService(client)
 
-	p, err := projectService.GetSettings("circleci", "8e4z1Akd74woxagxnvLT5q", "V29Cenkg8EaiSZARmWm8Lz")
+	name := "test-api-client-repo"
+	organization_id := "3ddcf1d1-7f5f-4139-8cef-71ad0921a968"
+	p, err := projectService.Create(name, organization_id)
 	if err != nil {
 		t.Log(err)
-		t.Error("Error getting project settings")
+		t.Error("Error creating project")
 		t.FailNow()
 	}
 	t.Log(p)
+	
+	project_settings, err := projectService.GetSettings("circleci", p.OrganizationId, p.Id)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error getting settings from project")
+		t.FailNow()
+	}
+	t.Log(project_settings)
+	new_settings := project.ProjectSettings{
+		Advanced: project.AdvanceSettings{
+			AutocancelBuilds: true,
+			DisableSSH: false,
+		},
+	}
+	project_settings, err = projectService.UpdateSettings(new_settings, "circleci", p.OrganizationId, p.Id)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error updating project settings")
+		t.FailNow()
+	}
+	t.Log(project_settings)
+
+	err = projectService.Delete(p.Slug)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error deleting project")
+		t.FailNow()
+	}
+}
+
+// This test is for manual usage only
+func _TestClassicProject(t *testing.T) {
+	token := os.Getenv("CIRCLECI_CLI_TOKEN")
+	if token == "" {
+		t.Error("Error: Token not found")
+	}
+	client := client.NewClient("https://circleci.com/api/v2", token)
+	projectService := project.NewProjectService(client)
+
+	name := "test-api-client-repo"
+	organization_id := "30361eb7-0812-447f-bca5-a299d96576c7"
+	_, err := projectService.Create(name, organization_id)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error creating project")
+		t.FailNow()
+	}
+}
+
+// This test is for manual usage only
+func _TestDeleteProject(t *testing.T) {
+	token := os.Getenv("CIRCLECI_CLI_TOKEN")
+	if token == "" {
+		t.Error("Error: Token not found")
+	}
+	client := client.NewClient("https://circleci.com/api/v2", token)
+	projectService := project.NewProjectService(client)
+
+	err := projectService.Delete("github/marboledacci/test-api-client-repo")
+	if err != nil {
+		t.Log(err)
+		t.Error("Error deleting project")
+		t.FailNow()
+	}
 }
