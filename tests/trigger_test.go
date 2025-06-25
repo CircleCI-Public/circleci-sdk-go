e15b93d06e21f8ba986d362de5598a2fe73e9324
marboledacci <mateo.arboleda@circleci.com>
2025-06-25 09:58:28 -0500

    Fix schema of new trigger type and add tests (#10)

--- tests/trigger_test.go
+++ tests/trigger_test.go
@@ -17,7 +17,7 @@ func TestListTrigger(t *testing.T) {
 	client := client.NewClient("https://circleci.com/api/v2", token)
 	triggerService := trigger.NewTriggerService(client)
 
-	pipeline_id := "f51dd4e5-11fe-4069-adad-0df0a7493d53"
+	pipeline_id := "bee796a0-7ec2-478c-ab87-6a5039d7a216"
 	project_id := "e2e8ae23-57dc-4e95-bc67-633fdeb4ac33"
 	trs, err := triggerService.List(project_id, pipeline_id)
 	if err != nil {
@@ -28,6 +28,76 @@ func TestListTrigger(t *testing.T) {
 	t.Log(trs)
 }
 
+func TestFullTriggerNew(t *testing.T) {
+	token := os.Getenv("CIRCLECI_CLI_TOKEN")
+	if token == "" {
+		t.Error("Error: Token not found")
+	}
+	client := client.NewClient("https://circleci.com/api/v2", token)
+	triggerService := trigger.NewTriggerService(client)
+
+	pipeline_id := "bee796a0-7ec2-478c-ab87-6a5039d7a216"
+	project_id := "e2e8ae23-57dc-4e95-bc67-633fdeb4ac33"
+	new_trigger := trigger.Trigger{
+		EventName: "Event Name",
+		EventSource: common.EventSource{
+			Provider: "webhook",
+			Webhook: common.Webhook{
+				Sender: "Test",
+			},
+		},
+		EventPreset: "all-pushes",
+		ConfigRef: "main",
+		CheckoutRef: "main",
+	}
+	trigger_created, err := triggerService.Create(new_trigger, project_id, pipeline_id)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error creating trigger")
+		t.FailNow()
+	}
+	if err != nil {
+		t.Log(err)
+		t.Error("Error creating trigger")
+		t.FailNow()
+	}
+	id_new_trigger := trigger_created.ID
+	trigger_to_update := trigger.Trigger{
+		Name: "trigger-updated",
+		Description: "Updated description",
+	}
+	trigger_updated, err := triggerService.Update(trigger_to_update, project_id, id_new_trigger)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error updating trigger")
+		t.FailNow()
+	}
+	if trigger_updated.Description != "Updated description" {
+		t.Error("Trigger was not updated")
+		t.FailNow()
+	}
+	trigger_fetched, err := triggerService.Get(project_id, id_new_trigger)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error getting trigger")
+		t.FailNow()
+	}
+	t.Log(trigger_fetched)
+	err = triggerService.Delete(project_id,id_new_trigger)
+	if err != nil {
+		t.Log(err)
+		t.Error("Error deleting trigger")
+		t.FailNow()
+	}
+	trigger_fetched, err = triggerService.Get(project_id,id_new_trigger)
+	t.Log(err)
+	if trigger_fetched != nil {
+		t.Error("Trigger was not deleted")
+		t.FailNow()
+	}
+}
+
+
 func TestFullTrigger(t *testing.T) {
 	token := os.Getenv("CIRCLECI_CLI_TOKEN")
 	if token == "" {
@@ -36,7 +106,7 @@ func TestFullTrigger(t *testing.T) {
 	client := client.NewClient("https://circleci.com/api/v2", token)
 	triggerService := trigger.NewTriggerService(client)
 
-	pipeline_id := "f51dd4e5-11fe-4069-adad-0df0a7493d53"
+	pipeline_id := "bee796a0-7ec2-478c-ab87-6a5039d7a216"
 	project_id := "e2e8ae23-57dc-4e95-bc67-633fdeb4ac33"
 	new_trigger := trigger.Trigger{
 		Name: "test-trigger",
