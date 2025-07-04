3cfdb072828147a0c6642ee0698287175ce2746a
Pete Steyert-Woods <pete.steyertwoods@circleci.com>
2025-07-04 10:22:22 +0100

    Tweak client some more

--- internal/testing/fakecircle/fakecircle.go
+++ internal/testing/fakecircle/fakecircle.go
@@ -2,11 +2,15 @@ package fakecircle
 
 import (
 	"net/http"
+	"sync/atomic"
 )
 
 type Service struct {
 	http.Handler
 	tok string
+
+	hit429 atomic.Bool
+	hit500 atomic.Bool
 }
 
 func New(tok string) *Service {
@@ -15,19 +19,51 @@ func New(tok string) *Service {
 	}
 
 	mux := http.NewServeMux()
-	c.Handler = c.auth(mux)
+	c.Handler = mux
+	c.Handler = c.auth(c.Handler)
+	c.Handler = loggingHandler(c.Handler)
 
-	mux.HandleFunc("GET /api/test", c.getTest)
+	mux.HandleFunc("GET /api/test/hello", c.getHello)
+	mux.HandleFunc("POST /api/test/echo", c.postEcho)
+	mux.HandleFunc("GET /api/test/429", c.get429)
+	mux.HandleFunc("GET /api/test/500", c.get500)
 	// TODO: More routes here
-	mux.HandleFunc("POST /api/retry", c.retryTest)
+
 	return c
 }
 
-func (s *Service) getTest(w http.ResponseWriter, _ *http.Request) {
+func (s *Service) getHello(w http.ResponseWriter, _ *http.Request) {
 	msg(w, http.StatusOK, "Hello World!")
 }
 
-// TODO: More routes here
-func (s *Service) retryTest(w http.ResponseWriter, _ *http.Request) {
-	msg(w, http.StatusGatewayTimeout, "Timeout")
+func (s *Service) postEcho(w http.ResponseWriter, r *http.Request) {
+	var body map[string]any
+	err := decode(r.Body, &body)
+	if err != nil {
+		msg(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	jsonBody(w, body)
 }
+
+func (s *Service) get429(w http.ResponseWriter, _ *http.Request) {
+	if !s.hit429.Swap(true) {
+		w.Header().Set("Retry-After", "1")
+		msg(w, http.StatusTooManyRequests, "Too many requests.")
+		return
+	}
+
+	msg(w, http.StatusOK, "Successfully retried.")
+}
+
+func (s *Service) get500(w http.ResponseWriter, _ *http.Request) {
+	if !s.hit500.Swap(true) {
+		msg(w, http.StatusInternalServerError, "Internal server error.")
+		return
+	}
+
+	msg(w, http.StatusOK, "Successfully retried.")
+}
+
+// TODO: More routes here
--- internal/testing/fakecircle/response.go
+++ internal/testing/fakecircle/response.go
@@ -2,7 +2,10 @@ package fakecircle
 
 import (
 	"encoding/json"
+	"io"
 	"net/http"
+
+	"github.com/CircleCI-Public/circleci-sdk-go/internal/closer"
 )
 
 func msg(w http.ResponseWriter, statusCode int, message string) {
@@ -12,6 +15,15 @@ func msg(w http.ResponseWriter, statusCode int, message string) {
 	}{
 		Message: message,
 	})
+}
+
+func decode(r io.ReadCloser, v any) (err error) {
+	defer closer.ErrorHandler(r, &err)
+	defer func() {
+		_, _ = io.Copy(io.Discard, r)
+	}()
+
+	return json.NewDecoder(r).Decode(v)
 }
 
 func jsonBody(w http.ResponseWriter, v any) {
