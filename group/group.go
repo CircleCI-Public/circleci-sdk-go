c44b49d8ea6cb703b974831c314af65fc4a80d1e
Mateo Arboleda <mateo.arboleda@circleci.com>
2025-11-27 13:04:09 -0500

    Add group resource

---
+++ group/group.go
@@ -0,0 +1,69 @@
+package group
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/CircleCI-Public/circleci-sdk-go/client"
+	"github.com/CircleCI-Public/circleci-sdk-go/common"
+)
+
+type Group struct {
+	ID           string `json:"id"`
+	Name         string `json:"name"`
+	Description  string `json:"description"`
+}
+
+type GroupService struct {
+	client *client.Client
+}
+
+func NewGroupService(c *client.Client) *GroupService {
+	return &GroupService{client: c}
+}
+
+
+func (s *GroupService) Get(ctx context.Context, orgID, groupID string) (_ *Group, err error) {
+	var group Group
+	_, err = s.client.RequestHelper(ctx, http.MethodGet, fmt.Sprintf("/organizations/%s/groups/%s", orgID, groupID), nil, &group)
+	if err != nil {
+		return nil, err
+	}
+
+	return &group, nil
+}
+
+func (s *GroupService) List(ctx context.Context, orgID string) (_ []Group, err error) {
+	var nextPageToken string
+	var groupsList []Group
+	for {
+		var response common.PaginatedResponse[Group]
+		_, err = s.client.RequestHelper(ctx, http.MethodGet, fmt.Sprintf("/organizations/%s/groups?page-token=%s", orgID, nextPageToken), nil, &response)
+		if err != nil {
+			return nil, err
+		}
+
+		groupsList = append(groupsList, response.Items...)
+		if response.NextPageToken == "" {
+			break
+		}
+		nextPageToken = response.NextPageToken
+	}
+	return groupsList, nil
+}
+
+func (s *GroupService) Create(ctx context.Context, newGroup Group, orgID string) (_ *Group, err error) {
+	var group Group
+	_, err = s.client.RequestHelper(ctx, http.MethodPost, fmt.Sprintf("/organizations/%s/groups", orgID), newGroup, &group)
+	if err != nil {
+		return nil, err
+	}
+
+	return &group, nil
+}
+
+func (s *GroupService) Delete(ctx context.Context, orgID, groupID string) (err error) {
+	_, err = s.client.RequestHelper(ctx, http.MethodDelete, fmt.Sprintf("/organizations/%s/groups/%s", orgID, groupID), nil, nil)
+	return err
+}
---
+++ group/group_test.go
@@ -0,0 +1,50 @@
+package group
+
+import (
+	"context"
+	"testing"
+
+	"gotest.tools/v3/assert"
+	"gotest.tools/v3/assert/cmp"
+
+	"github.com/CircleCI-Public/circleci-sdk-go/internal/testing/integrationtest"
+)
+
+func TestFullGroup(t *testing.T) {
+	ctx := context.TODO()
+	c := integrationtest.Client(t)
+	groupService := NewGroupService(c)
+
+	orgID := "3ddcf1d1-7f5f-4139-8cef-71ad0921a968"
+
+	newGroup := Group{
+		Name:        "test-group",
+		Description: "Test group from SDK",
+	}
+	groupCreated, err := groupService.Create(ctx, newGroup, orgID)
+	assert.Assert(t, err)
+
+	idNewGroup := groupCreated.ID
+
+	groupFetched, err := groupService.Get(ctx, orgID, idNewGroup)
+	assert.Assert(t, err)
+	assert.Check(t, groupFetched != nil)
+
+	err = groupService.Delete(ctx, orgID, idNewGroup)
+	assert.Assert(t, err)
+
+	groupFetched, err = groupService.Get(ctx, orgID, idNewGroup)
+	assert.Assert(t, err != nil)
+	assert.Check(t, cmp.Nil(groupFetched))
+}
+
+func TestListGroup(t *testing.T) {
+	ctx := context.TODO()
+	c := integrationtest.Client(t)
+	groupService := NewGroupService(c)
+
+	orgID := "3ddcf1d1-7f5f-4139-8cef-71ad0921a968"
+	groups, err := groupService.List(ctx, orgID)
+	assert.Assert(t, err)
+	assert.Assert(t, cmp.Len(groups, 1))
+}
